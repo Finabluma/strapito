8e7d9704c80071fb3b16140a70a1cedb2ff99cef
finabluma <oscar.francino@finabluma.com>
2022-06-19 17:35:03 +0200

    removed advanced configuration from dev server

--- config/server.js
+++ config/server.js
@@ -4,9 +4,4 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED'),
-  },
 });
