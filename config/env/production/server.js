d1ecfba5e41de1820947dd71027c426ede4cdceb
finabluma <oscar.francino@finabluma.com>
2022-06-19 12:15:38 +0200

    .

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,4 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL'),
+  proxy: env.bool('IS_PROXIED'),
+  cron: {
+    enabled: env.bool('CRON_ENABLED'),
+  },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env.int('PORT'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED'),
   },
 });
