d83e211bce0f2ba30ba90076fe1793bf90144b15
finabluma <oscar.francino@finabluma.com>
2022-06-21 14:11:20 +0200

    .

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,32 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
+        delete: {},
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
   ckeditor: {
     enabled: true,
     config: {
@@ -91,38 +119,4 @@ module.exports = () => ({
     }
   },
 });
-
-module.exports = () => ({
-  graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
-});
-
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        uploadStream: {
-          folder: env("CLOUDINARY_FOLDER"),
-        },
-        delete: {},
-      },
-    },
-  },
-})
 
