fe89773041c502095d193d7c73defdb8b346c712
finabluma <oscar.francino@finabluma.com>
2022-06-21 18:10:43 +0200

    bug fix

--- config/plugins.js
+++ config/plugins.js
@@ -16,15 +16,13 @@ module.exports = ({ env }) => ({
     },
   },
   graphql: {
+    enabled: true,
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
+      defaultLimit: 30,
+      maxLimit: 50,
     },
   },
   ckeditor: {
